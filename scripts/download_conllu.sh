da0adc49bb039b5a2415e8515f911666501bbe87
ljvmiranda921 <ljvmiranda@gmail.com>
2024-12-02 10:13:53 -0800

    Add script to download CoNLL-u files from UD

---
+++ scripts/download_conllu.sh
@@ -0,0 +1,4 @@
+echo "Downloading files from the source..."
+wget -O assets/tl_newscrawl-ud-train.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-train.conllu
+wget -O assets/tl_newscrawl-ud-dev.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-dev.conllu
+wget -O assets/tl_newscrawl-ud-test.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-test.conllu
