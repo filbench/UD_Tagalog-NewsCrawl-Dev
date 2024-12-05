d389fb2d5d81a6ad5302385cf4e0dd96e3348ae8
ljvmiranda921 <ljvmiranda@gmail.com>
2024-12-04 20:59:54 -0800

    Create assets directory

--- scripts/download_conllu.sh
+++ scripts/download_conllu.sh
@@ -7,6 +7,7 @@ do
     esac
 done
 echo "Downloading files from the source..."
+mkdir -p assets
 wget -O assets/tl_newscrawl-ud-train.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-train.conllu
 wget -O assets/tl_newscrawl-ud-dev.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-dev.conllu
 wget -O assets/tl_newscrawl-ud-test.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-test.conllu
