a7b83d2282e38ae785f57909b92a0e1896a32e71
Lj Miranda <12949683+ljvmiranda921@users.noreply.github.com>
2024-12-04 21:20:08 -0800

    Update README (#1)

---
+++ README.md
@@ -0,0 +1,21 @@
+<img src="https://cdn-avatars.huggingface.co/v1/production/uploads/634e20a0c1ce28f1de920cc4/k7SJny1M3lDa5CH_T1bp3.png" width="130" height="130" align="right" />
+
+# UD_Tagalog-NewsCrawl (Dev)
+
+This repository contains several experiments and benchmarks for the paper [_UD-NewsCrawl: A Universal Dependencies Treebank for Tagalog_]() which introduces [UD_Tagalog-NewsCrawl](https://huggingface.co/datasets/UD-Filipino/UD_Tagalog-NewsCrawl), the largest Tagalog treebank to date.
+
+## Downloading the dataset
+
+The dataset is available as a set of CoNLL-U files which can be downloaded directly from Universal Dependencies:
+
+```sh
+bash scripts/download_conllu.sh dev
+```
+
+You can also download it as a HuggingFace dataset:
+
+```python
+# pip install datasets
+from datasets import load_dataset
+ds = load_dataset("UD-Filipino/UD_Tagalog-NewsCrawl")
+```
--- scripts/download_conllu.sh
+++ scripts/download_conllu.sh
@@ -1,4 +1,13 @@
+branch="dev"
+
+while getopts b: flag
+do
+    case "${flag}" in
+        b) branch=${OPTARG};;
+    esac
+done
 echo "Downloading files from the source..."
-wget -O assets/tl_newscrawl-ud-train.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-train.conllu
-wget -O assets/tl_newscrawl-ud-dev.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-dev.conllu
-wget -O assets/tl_newscrawl-ud-test.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-test.conllu
+mkdir -p assets
+wget -O assets/tl_newscrawl-ud-train.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-train.conllu
+wget -O assets/tl_newscrawl-ud-dev.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-dev.conllu
+wget -O assets/tl_newscrawl-ud-test.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-test.conllu
