03605e4770fe35150402d2a2673b3f25f53ac72d
ljvmiranda921 <ljvmiranda@gmail.com>
2024-12-04 20:59:14 -0800

    Parametrize the branch

--- scripts/download_conllu.sh
+++ scripts/download_conllu.sh
@@ -1,4 +1,12 @@
+branch="dev"
+
+while getopts b: flag
+do
+    case "${flag}" in
+        b) branch=${OPTARG};;
+    esac
+done
 echo "Downloading files from the source..."
-wget -O assets/tl_newscrawl-ud-train.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-train.conllu
-wget -O assets/tl_newscrawl-ud-dev.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-dev.conllu
-wget -O assets/tl_newscrawl-ud-test.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/dev/tl_newscrawl-ud-test.conllu
+wget -O assets/tl_newscrawl-ud-train.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-train.conllu
+wget -O assets/tl_newscrawl-ud-dev.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-dev.conllu
+wget -O assets/tl_newscrawl-ud-test.conllu https://raw.githubusercontent.com/UniversalDependencies/UD_Tagalog-NewsCrawl/refs/heads/$branch/tl_newscrawl-ud-test.conllu
